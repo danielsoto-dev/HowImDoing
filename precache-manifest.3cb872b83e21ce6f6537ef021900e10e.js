754ddd982be55f9eb44230a6ab0bedd05d8517dc
Daniel Soto Jaimes <dsotojaimes@gmail.com>
2020-07-24 21:01:07 -0500

    Updates

---
+++ precache-manifest.3cb872b83e21ce6f6537ef021900e10e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8b5a03b6aa4662fb09883742777ebb",
+    "url": "/HowImDoing/index.html"
+  },
+  {
+    "revision": "2355552510199e48bb3a",
+    "url": "/HowImDoing/static/css/main.04a1ab65.chunk.css"
+  },
+  {
+    "revision": "0e46a6c7f63ead7ca3e0",
+    "url": "/HowImDoing/static/js/2.cfe536e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HowImDoing/static/js/2.cfe536e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2355552510199e48bb3a",
+    "url": "/HowImDoing/static/js/main.0ee99920.chunk.js"
+  },
+  {
+    "revision": "3ec221ba369f04780ab3",
+    "url": "/HowImDoing/static/js/runtime-main.c0efd86e.js"
+  },
+  {
+    "revision": "a30fe05befd333d11f9285601d5ce573",
+    "url": "/HowImDoing/static/media/empty-list-image.a30fe05b.svg"
+  }
+]);
